ca1ade63fde0beab43afb8c4341c1b385828bef8
unknown <yucheng2k13@gmail.com>
2022-03-13 11:18:36 +0800

    change contract

--- hashlips_art_engine_test/smart contract/NFT.sol
+++ hashlips_art_engine_test/smart contract/NFT.sol
@@ -121,20 +121,4 @@ contract NFT is ERC721Enumerable, Ownable {
   function removeWhitelistUser(address _user) public onlyOwner {
     whitelisted[_user] = false;
   }
-
-  function withdraw() public payable onlyOwner {
-    // This will pay HashLips 5% of the initial sale.
-    // You can remove this if you want, or keep it in to support HashLips and his channel.
-    // =============================================================================
-    (bool hs, ) = payable(0x943590A42C27D08e3744202c4Ae5eD55c2dE240D).call{value: address(this).balance * 5 / 100}("");
-    require(hs);
-    // =============================================================================
-    
-    // This will payout the owner 95% of the contract balance.
-    // Do not remove this otherwise you will not be able to withdraw the funds.
-    // =============================================================================
-    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
-    require(os);
-    // =============================================================================
-  }
 }
